886128dba74dd1f5bbcd3709bea2622d8c0257c7
Jhonatan Quihuiri <jhquihuiri7@localhost.localdomain>
2024-01-24 18:44:58 -0600

    Metadata added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import { Helmet } from 'react-helmet';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Jhonatan Quihuiri',
-  description: '',
+  description: 'Welcome to my developer portfolio! I am a passionate and dedicated professional with expertise in backend development, mobile app development, and data analysis. The world of finance, trading, programming, and data fuels my curiosity and drives me to excel in my academic and professional responsibilities.',
 
 }
 
@@ -17,12 +17,6 @@ export default function RootLayout({
 }) {
   return (
       <html lang="en">
-      <head>
-          <meta
-              name="description"
-              content="Welcome to my developer portfolio! I am a passionate and dedicated professional with expertise in backend development, mobile app development, and data analysis. The world of finance, trading, programming, and data fuels my curiosity and drives me to excel in my academic and professional responsibilities."
-          />
-      </head>
       <body className={inter.className}>{children}</body>
       </html>
   )

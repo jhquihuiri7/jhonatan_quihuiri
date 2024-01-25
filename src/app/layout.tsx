4936f30311597fab525da8d0838f7f16dce104d2
Jhonatan Quihuiri <jhquihuiri7@localhost.localdomain>
2024-01-24 18:39:44 -0600

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Helmet } from 'react-helmet';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,10 +17,12 @@ export default function RootLayout({
 }) {
   return (
       <html lang="en">
-      <meta
-          name="description"
-          content="Welcome to my developer portfolio! I am a passionate and dedicated professional with expertise in backend development, mobile app development, and data analysis. The world of finance, trading, programming, and data fuels my curiosity and drives me to excel in my academic and professional responsibilities."
-      />
+      <head>
+          <meta
+              name="description"
+              content="Welcome to my developer portfolio! I am a passionate and dedicated professional with expertise in backend development, mobile app development, and data analysis. The world of finance, trading, programming, and data fuels my curiosity and drives me to excel in my academic and professional responsibilities."
+          />
+      </head>
       <body className={inter.className}>{children}</body>
       </html>
   )
